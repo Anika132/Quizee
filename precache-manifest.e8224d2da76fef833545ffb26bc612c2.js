dc2213e9a916c2706e7ef738b3ba6eb71ff685bf
Anika <anikasharma132@gmail.com>
2019-10-12 19:52:54 +0530

    Updates

---
+++ precache-manifest.e8224d2da76fef833545ffb26bc612c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adb56772c94bd96cfdae8d2b9f49ca12",
+    "url": "/Quizee/index.html"
+  },
+  {
+    "revision": "b00ec8e7492ef85d4313",
+    "url": "/Quizee/static/css/main.f293264b.chunk.css"
+  },
+  {
+    "revision": "88f923c3e581ceb83f82",
+    "url": "/Quizee/static/js/2.e31b99bb.chunk.js"
+  },
+  {
+    "revision": "b00ec8e7492ef85d4313",
+    "url": "/Quizee/static/js/main.394bd9f7.chunk.js"
+  },
+  {
+    "revision": "20d57905c6d11a4f1ec5",
+    "url": "/Quizee/static/js/runtime-main.f5aa8cd2.js"
+  },
+  {
+    "revision": "296126d23716c29dd4c6d1569a4ad9df",
+    "url": "/Quizee/static/media/1.296126d2.png"
+  },
+  {
+    "revision": "4d50ff3630e88ca81ecdf05511d162d3",
+    "url": "/Quizee/static/media/t3.4d50ff36.jpg"
+  }
+]);
